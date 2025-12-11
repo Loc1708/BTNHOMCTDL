c92f99b76e57fbb626614bfcae923001a5761431
Loc1708 <loclethien721@gmail.com>
2025-12-11 20:44:25 +0700

    Refactor sorting algorithms and optimize bucket sort
    Optimized bucket sort and improved sorting functions.

--- btnhom_sort.cpp
+++ btnhom_sort.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <climits>
@@ -145,53 +145,35 @@ void readData(SinhVien**& a, int*& sz) {
 }
 
 
-//so sanh mssv;
-ll cmpMSSV(SinhVien a, SinhVien b) {
-	return a.mssv - b.mssv;
-}
 
-//so sanh do dai cua ten
-ll cmpTen(SinhVien a, SinhVien b) {
-	return a.hoten.len - b.hoten.len;
-}
-
-//so sanh diem trung binh
-ll cmpDTB(SinhVien a, SinhVien b) {
-	return a.dtb - b.dtb > 0 ? 1 : a.dtb - b.dtb < 0 ? -1 : 0;
-}
-
-//so sanh hoc phi
-ll cmpHP(SinhVien a, SinhVien b) {
-	return a.hp - b.hp > 0 ? 1 : a.hp - b.hp < 0 ? -1 : 0;
-}
-
-void shellSort(SinhVien*& a, int n, ll func(SinhVien a, SinhVien b), int chon2) {
-	for (int gap = n / 2; gap > 0; gap /= 2) {
-		for (int i = gap; i < n; i++) {
-			SinhVien tmp = a[i];
-			int j;
-			switch (chon2)
+void shellSort(SinhVien*& a, int n, int mode, int mode2) {
+	
+	for (int gap = n / 2; gap > 0; gap /= 2)
+	{
+		for (int i = gap; i < n; i++)
+		{
+			ll cur = getVal(a[i], mode);
+			SinhVien curt = a[i];
+			int pos = i;
+			while (pos >= gap && getVal(a[pos - gap], mode) > cur)
 			{
-			case 1: {
-				for (j = i; j >= gap && func(a[j - gap], tmp) > 0; j -= gap) {
-					a[j] = a[j - gap];
-				}
-				break;
-
+				a[pos] = a[pos - gap];
+				pos -= gap;
 			}
-			case 2: {
-				for (j = i; j >= gap && func(a[j - gap], tmp) < 0; j -= gap) {
-					a[j] = a[j - gap];
-				}
-				break;
-			}
-			default:
-				break;
-			}
-			a[j] = tmp;
+			a[pos] = curt;
 		}
-
 	}
+	if (mode2 == 2)
+	{
+		int l = 0; int r = n - 1;
+		while (l < r)
+		{
+			swap(a[l], a[r]);
+			l++;
+			r--;
+		}
+	}
+	
 
 }
 
@@ -263,6 +245,21 @@ void radixSort(SinhVien*& a, int n, int chon1, int chon2) {
 
 }
 
+ll divide(ll x)
+{
+	ll sum = 1;
+	while (x != 0)
+	{
+		sum *= 10;
+		x /= 10;
+	}
+	return sum/10;
+}
+
+
+
+
+// tối ưu lại bucketsort
 void bucketSort(SinhVien*& a, int n, int chon1, int chon2) {
 	SinhVien** bucket = new SinhVien * [n];
 	int* sz = new int[n];
@@ -271,19 +268,25 @@ void bucketSort(SinhVien*& a, int n, int chon1, int chon2) {
 		sz[i] = 0;
 	}
 
+	ll m = findMax(a, n, chon1);
+	
+	ll div = divide(m);
+	if (div == 0) div = 1;
 	for (int i = 0; i < n; i++) {
-		int idx = 0;
-		push(bucket[idx], sz[idx], a[i]);
+		ll bucketI = getVal(a[i], chon1) / div;
+		if (bucketI >= n) bucketI = n - 1;
+
+		push(bucket[bucketI], sz[bucketI], a[i]);
 	}
 
 	for (int i = 0; i < n; i++) {
-		if (bucket[i] == nullptr)continue;
-		radixSort(bucket[i], sz[i], chon1, chon2);
+		if (sz[i] <= 1) continue;    
+		radixSort(bucket[i], sz[i], chon1, 1);
 	}
 
 
 	int idx = 0;
-	/*if (chon2 == 1) {*/
+	
 	for (int i = 0; i < n; i++) {
 		if (bucket[i] == nullptr)continue;
 		for (int j = 0; j < sz[i]; j++) {
@@ -291,16 +294,21 @@ void bucketSort(SinhVien*& a, int n, int chon1, int chon2) {
 			idx++;
 		}
 	}
-	/*}
-	else  {
-		for (int i = n - 1; i >= 0; i--) {
-			if (bucket[i] == nullptr)continue;
-			for (int j = sz[i]-1; j >= 0 ; j--) {
-				a[idx] = bucket[i][j];
-				idx++;
-			}
+	if (chon2 == 2)
+	{
+		int l = 0; int r = n - 1;
+		while (l < r)
+		{
+			swap(a[l], a[r]);
+			l++;
+			r--;
 		}
-	}*/
+	}
+	for (int i = 0; i < n; i++) {
+		delete[] bucket[i];
+	}
+	delete[] bucket;
+	delete[] sz;
 
 }
 
@@ -331,14 +339,14 @@ void process(SinhVien**& a, int*& sz, int chon, int chon1, int chon2, List& l) {
 			switch (chon1)
 			{
 			case 1: {
-				shellSort(l.list, l.n, cmpMSSV, chon2);
+				shellSort(l.list, l.n, chon1,chon2);
 				break;
 			}
 			case 3: {
-				shellSort(l.list, l.n, cmpDTB, chon2);
+				shellSort(l.list, l.n, chon1,chon2);
 				break;
 			case 4: {
-				shellSort(l.list, l.n, cmpHP, chon2);
+				shellSort(l.list, l.n, chon1,chon2);
 				break;
 			}
 			}
@@ -370,7 +378,7 @@ void process(SinhVien**& a, int*& sz, int chon, int chon1, int chon2, List& l) {
 			switch (chon)
 			{
 			case 1: {
-				shellSort(a[i], sz[i], cmpTen, chon2);
+				shellSort(a[i], sz[i], chon1,chon2);
 
 				break;
 			}
@@ -415,9 +423,11 @@ int main() {
 
 	//luu tru du lieu bang mang khi nguoi dung chon sort theo mssv,diem trung binh,hoc phi
 	List l;
+
+
 	
-
-
+	
+	cout << "========================================\n";
 	int c;
 	do
 	{
@@ -458,6 +468,7 @@ int main() {
 
 		cout << "Tiep tuc ( bam 1 ) / ket thuc( bam 0 ): ";
 		cin >> c;
+		
 	} while (c == 1);
 
 
@@ -469,5 +480,4 @@ int main() {
 	delete[]a;
 	delete[]sz;
 	return 0;
-
 }
